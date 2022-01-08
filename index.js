bb474688302223414f47f6d2d202ac63d8aa366e
alsadig-ahmed <alsadq.ahmd@gmail.com>
2022-01-08 15:49:02 +0200

    add static folder + npm script

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 const port = process.env.PORT || 3000 ;
 const quotesRouter = require('./routes/quotes');
 
 app.use(express.json());
+app.use(express.urlencoded({extended:true})) // this is to make 
 
-app.get('/', (req, res) => {
-  res.json({message: `alive Example app listening at http://localhost:${port}`});
+
+
+// static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/port', (req, res) => {
+  res.json({message: `alive Example app listening on port:${port}`});
 });
-
 app.use('/quotes', quotesRouter);
 
 app.listen(port, () => {
